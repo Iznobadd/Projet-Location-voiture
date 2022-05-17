809c9644c5a05ccb77ddf55dee2a9330e2ada980
Brandon CEBI <brandon.cebi.pro@gmail.com>
2022-05-17 11:23:51 +0200

    Add cards

--- cards.php
+++ cards.php
@@ -1,38 +1,27 @@
-<div class="row row-cols-1 row-cols-md-3 g-4">
-    <div class="col">
-        <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+<?php
+$filename = "cars.json";
+$data = file_get_contents($filename);
+$cars = json_decode($data);
+?>
+
+<div class="bd-example">
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        <?php
+            foreach($cars as $car){
+        ?>
+        <div class="col">
+            <div class="card">
+                <img class="bd-placeholder-img card-img-top" width="100%" height="200" src="<?= $car->url_image ?>" alt="" role="img">
+                <div class="card-body">
+                    <h5 class="card-title"><?= $car->model ?></h5>
+                    <p class="card-text"><?= $car->description ?></p>
+                    <h5 class="card-title">Nombre de places : <?= $car->place ?></h5>
+                    <h5 class="card-title">Tarif journalier : <?= $car->tarif ?>€</h5>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col">
-        <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-        </div>
-    </div>
-    <div class="col">
-        <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-            </div>
-        </div>
-    </div>
-    <div class="col">
-        <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-        </div>
+        <?php
+            }
+        ?>
     </div>
 </div>
